a5185432a30e8426926479858580d86a70b1a24d
Kruguinn <162449721+Kruguinn@users.noreply.github.com>
2025-09-02 12:17:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
+// npm init
+// npm i express
 const express = require("express")
 const app = express()
 const port = 3000
 app.use(express.json())
 
-app.get("/bemvindo",(req, res)=> {
-    res.send("Olá, seja bem vindo a API")
+// npm i mysql2
+const db = require("./db")
+
+app.post ("/cadastrar", async (req, res) => {
+    const cliente = req.body
+    try {
+        const resultado = await db.pool.query(
+            `INSERT INTO cliente (nome, cpf, email,
+            telefone, rua, n_casa, bairro, cidade, uf, cep, senha)
+            values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+
+            [cliente.nome, cliente.cpf, cliente.email,
+                cliente,telefone, cliente.rua, cliente.n_casa,
+                cliente.bairro, cliente.cidade, cliente.uf, cliente.cep, cliente.senha])
+
+            res.status(500).json ({erro: "Erro inteiro na API"})
+    } catch (erro) {
+        res.status(500).json({erro: "Erro interno API"})
+    }
 })
 
-app.post("/bemvindo",(req, res)=> {
-    const dados = req.body
-    res.send("Olá, seja bem vindo " + dados.nome)
-    })
+app.get("/clientes", async (req,res) => {
+    try {
+        const resultado = await db.pool.query("SELECT * FROM cliente")
+        res.status(200).json(resultado[0])
+    } catch (erro){
+        console.log(erro)
+    }
+})
 
 app.listen(port, ()=>{
-    console.log("API RODANDO NA PORTA " + port)
+    console.log("API executando na porta " + port)
 })
